ec5f40261d3ea86406099602b72f7ad247cfe321
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-06-16 09:30:36 -0400

    My take on assignment 3

--- Assignments/week1/menu/src/main/java/Menu.java
+++ Assignments/week1/menu/src/main/java/Menu.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Menu {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+
+        outer:
+        while (true) {
+            System.out.println("1) get a random number");
+            System.out.println("2) reverse a given string");
+            System.out.println("3) exit");
+            String input = sc.nextLine();
+
+            switch (input) {
+                case "1":
+                    System.out.println(Math.random() + "\n");
+                    break;
+                case "2":
+                    System.out.println("Enter string");
+                    StringBuilder s = new StringBuilder(sc.nextLine());
+                    s.reverse();
+                    System.out.println(s + "\n");
+                    break;
+                case "3":
+                    break outer;
+            }
+        }
     }
 }
