8b80ea9cb45dca45722d671e5c9f000ecdc21e8d
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-06-22 17:03:31 -0400

    Starting template for shop project.

---
+++ Projects/Shop/src/main/java/com/revature/Driver.java
@@ -0,0 +1,18 @@
+package com.revature;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        try {
+            // try to hack me :)
+            Connection c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456");
+            System.out.println(c.getMetaData().getDriverName());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
