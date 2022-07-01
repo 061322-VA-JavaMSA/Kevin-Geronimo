21c8fa44d02de715853d10926e0b0d34e0649285
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-07-01 18:02:37 -0400

    refactor(week3): move const variable out of for loop

--- assignments/week3/js-fetch/fetch.js
+++ assignments/week3/js-fetch/fetch.js
@@ -16,6 +16,27 @@
 */
 let baseApiURL = 'https://pokeapi.co/api/v2/pokemon';
 
+const colors = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+};
+
 async function getData() {
     console.log('Button was clicked!');
     let id = document.getElementById('dataInput').value;
@@ -64,27 +85,6 @@ function populateData(response) {
     response.types.forEach(typeinfo => {
         let button = document.createElement("button");
         button.innerHTML = typeinfo.type.name;
-        
-        const colors = {
-            normal: '#A8A77A',
-            fire: '#EE8130',
-            water: '#6390F0',
-            electric: '#F7D02C',
-            grass: '#7AC74C',
-            ice: '#96D9D6',
-            fighting: '#C22E28',
-            poison: '#A33EA1',
-            ground: '#E2BF65',
-            flying: '#A98FF3',
-            psychic: '#F95587',
-            bug: '#A6B91A',
-            rock: '#B6A136',
-            ghost: '#735797',
-            dragon: '#6F35FC',
-            dark: '#705746',
-            steel: '#B7B7CE',
-            fairy: '#D685AD',
-        };
 
         button.style.cssText = `
         border-radius: 4px;
